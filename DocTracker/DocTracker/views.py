f7797fa1c436db8aca64b2113e54ca4e870612b6
Teknath Jha <jhateknath64@gmail.com>
2021-09-21 15:35:47 +0530

    Project set up | first commit

---
+++ DocTracker/DocTracker/views.py
@@ -0,0 +1,28 @@
+import os
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.views import generic
+from django.views.generic.base import View
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password
+
+
+class landingPage(View):
+
+    def get(self, request, template_name='landingPage.html'):
+        try:
+            message = {'message': 'message'}
+            message['message'] = 'nothing'
+        except:
+            message = {'message': 'message'}
+            message['message'] = 'nothing'
+        return render(request, template_name, message)
+
+
+class about(View):
+    def get(self, request, template_name = 'about.html'):
+        return render(request, template_name)
---
+++ DocTracker/static/js/profile.js
@@ -0,0 +1,8 @@
+let organisation = document.getElementById("Salah").textContent
+document.getElementById("Mane").value = organisation
+
+let phoneNumber = document.getElementById("Dias").textContent
+document.getElementById("Stones").value = phoneNumber
+
+let address = document.getElementById("Son").textContent
+document.getElementById("Kane").value = address
